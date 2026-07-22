73e7d8ac23bcac9209931f4d784a7a36eb9e0cf0
Pedro Souza <mypedrosouza@gmail.com>
2026-07-22 11:09:27 -0300

    feat: bitstream and tANS (for 32 bits) based on StaticANS

---
+++ source/Common/TabledANS.h
@@ -0,0 +1,136 @@
+#pragma once
+#include <algorithm>
+#include <cstdint>
+#include <list>
+
+#include "Utils/Bitstream.h"
+
+
+class StateBitstream {
+public:
+    uint32_t size = 0;
+    uint32_t bitstream = 0;
+
+    StateBitstream() = default;
+
+    explicit StateBitstream(const uint32_t size, const uint32_t bitstream) : size(size), bitstream(bitstream) {
+    }
+
+    ~StateBitstream() = default;
+};
+
+class State {
+public:
+    uint32_t state = 0;
+    std::vector<uint32_t> nextStates = {};
+    std::vector<StateBitstream> bitstreams = {};
+
+    State() = default;
+
+    explicit State(const uint32_t state, const std::vector<uint32_t> &nextStates,
+                   const std::vector<StateBitstream> &bitstreams) : state(state), nextStates(nextStates),
+                                                                    bitstreams(bitstreams) {
+    }
+
+    ~State() = default;
+};
+
+class DecodeState {
+public:
+    std::uint8_t symbol = 255; //
+    std::uint32_t N = 0;
+    std::uint32_t base = 0;
+
+    DecodeState() = default;
+
+    explicit DecodeState(const std::int32_t symbol, const std::uint32_t N, const std::uint32_t base) : symbol(symbol),
+        N(N),
+        base(base) {
+    }
+
+    ~DecodeState() = default;
+};
+
+
+class Table {
+    uint32_t L = 0;
+
+    std::vector<State> states = {};
+    std::vector<DecodeState> decodeStates = {};
+
+public:
+    Table() = default;
+
+    explicit Table(const uint32_t L, const std::list<State> &states) {
+        // Temporary (.dat needs has an L number)
+        this->L = L;
+
+        this->states.resize(L);
+        for (const State &state: states) {
+            this->states[state.state - L] = state;
+        }
+
+        this->decodeStates.resize(L);
+        for (const auto &state: this->states) {
+            for (uint8_t symbol = 0; symbol < static_cast<uint8_t>(state.nextStates.size()); ++symbol) {
+                const uint32_t &nextState = state.nextStates[symbol];
+                if (state.bitstreams.empty() && this->decodeStates[nextState - L].symbol == 255) {
+                    this->decodeStates[nextState - L] = DecodeState(symbol, 0, state.state);
+                    continue;
+                }
+                const StateBitstream &stateBitstream = state.bitstreams.at(symbol);
+                if (this->decodeStates[nextState - L].symbol == 255) {
+                    this->decodeStates[nextState - L] = DecodeState(symbol, stateBitstream.size, state.state);
+                } else {
+                    DecodeState &decodeState = this->decodeStates[nextState - L];
+                    decodeState.base = std::min(decodeState.base, state.state);
+                }
+            }
+        }
+    }
+
+    ~Table() = default;
+
+    /**
+     * The first state to that table (initialize you state variable with that value)
+     * @return the first state
+     */
+    [[nodiscard]] uint32_t getFirstState() const {
+        return L;
+    }
+
+    /**
+     * Encoded symbol and write on bitstream
+     * @param currentState the current state
+     * @param symbol the symbol will be encoded
+     * @param writer the bitstream writer
+     * @return new state (as unsigned 16 bits)
+     */
+    void encode(uint32_t &currentState, const uint8_t symbol, BitstreamWriter &writer) const {
+        const State &state = states[currentState - L];
+        const uint32_t &nextState = state.nextStates.at(symbol);
+        const StateBitstream &bitstream = state.bitstreams.at(symbol);
+        writer.write(bitstream.size, bitstream.bitstream);
+        currentState = nextState;
+    }
+
+    /**
+     * Decode the simbol and write on bitstream
+     * @param currentState the current state
+     * @param reader the bitstream reader
+     * @return a pair of previous state and symbol value
+     */
+    uint8_t decode(uint32_t &currentState, BitstreamReader &reader) const {
+        const DecodeState &decodeState = this->decodeStates[currentState - L];
+
+        // A state without a bitstream
+        if (decodeState.N == 0) {
+            currentState = decodeState.base;
+            return decodeState.symbol;
+        }
+
+        const auto readBitstreams = reader.read(decodeState.N);
+        currentState = decodeState.base + readBitstreams;
+        return decodeState.symbol;
+    }
+};
---
+++ source/Utils/Bitstream.h
@@ -0,0 +1,117 @@
+#pragma once
+#include <cstdint>
+#include <stdexcept>
+#include <vector>
+
+class BitstreamWriter {
+    uint8_t ptr = 8;
+    uint8_t currentBitstream = 0;
+
+public:
+    // Using 8 bit bitstream and implemented as stack because of ANS characteristic (decode is inversed of encode)
+    std::vector<uint8_t> bitstream = {};
+
+    BitstreamWriter() = default;
+
+    ~BitstreamWriter() = default;
+
+    /**
+     * Write bits on bitstream
+     * @param size size of bitstream
+     * @param bits bitstream (0bXXX)
+     */
+    void write(const uint8_t size, const uint32_t bits) {
+        for (int i = size - 1; i >= 0; i--) {
+            const uint8_t bit = (bits >> i) & 1;
+
+            ptr--;
+            currentBitstream = currentBitstream | (bit << ptr);
+
+            if (ptr == 0) {
+                bitstream.push_back(currentBitstream);
+                currentBitstream = 0;
+                ptr = 8;
+            }
+        }
+    }
+
+    /**
+     * Flush bitstream
+     * Call it when work with bitstream was ended
+     * @return offset to read bitstream
+     */
+    uint8_t flush() {
+        uint8_t offset = 0;
+        if (ptr < 8) {
+            bitstream.push_back(currentBitstream);
+            offset = ptr;
+            currentBitstream = 0;
+            ptr = 8;
+        }
+        return offset;
+    }
+};
+
+class BitstreamReader {
+    uint32_t buffer = 0;
+    unsigned int count = 0;
+
+    /**
+     * Load the buffer with 4 bitstream
+     */
+    void refill() {
+        while (count <= 24 && !bitstream.empty()) {
+            const uint8_t nextBits = bitstream.back();
+            bitstream.pop_back();
+            buffer = buffer | (static_cast<uint32_t>(nextBits) << count);
+            count += 8;
+        }
+
+    }
+
+public:
+    std::vector<uint8_t> bitstream = {};
+
+    BitstreamReader() = default;
+
+    explicit BitstreamReader(std::vector<uint8_t> bs, const std::uint8_t offset) : bitstream(std::move(bs)) {
+        this->refill();
+        if (offset > 0 && offset < 8) {
+            this->read(offset);
+        }
+    }
+
+    /**
+     * Advance (remove) n bits from bitstream
+     * @param n number on bits
+     */
+    uint32_t read(const unsigned int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n > 31) {
+            throw std::invalid_argument("Can't read most 32 bits from bitstreams");
+        }
+
+
+        if (count < n) {
+            this->refill();
+
+            if (count < n) {
+                throw std::runtime_error("Bitstream underflow (not has " + std::to_string(n) + " bits to read)");
+            }
+        }
+
+        const uint32_t mask = (1U << n) - 1;
+        const uint32_t data = buffer & mask;
+
+        buffer = buffer >> n;
+        count -= n;
+
+        return data;
+    }
+
+    [[nodiscard]] unsigned int getCount() const {
+        return count;
+    }
+};
